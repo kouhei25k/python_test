66bd7bf223b84c3948190d39c0700441d6365d5e
kouhei <kouhei@DESKTOP-2LH624T>
2020-12-28 21:37:12 +0900

    python_test

--- test.py
+++ test.py
@@ -1,3 +1,28 @@
-"""
-このファイルに解答コードを書いてください
-"""
+# ファイル読み込み
+input_data = open('input.txt', 'r')
+
+# 整形
+input_data = input_data.readlines()
+
+input_m = int(input_data[-1:][0].rstrip('\n'))
+input_data = input_data[:-1]
+# print(input_m )
+
+formating_data = []
+for input_line in input_data:
+    input_line = input_line.split(':')
+    input_line[0] = int(input_line[0])
+    input_line[1] = input_line[1].rstrip('\n')
+    # print(input_line)
+    formating_data.append(input_line)
+# print(formating_data)
+sorted_formating_data = sorted(formating_data)
+
+# 出力
+output_string = ''
+for line in sorted_formating_data:
+    if input_m % line[0] == 0:
+        output_string += line[1]
+        # print(line)
+
+print(output_string)
